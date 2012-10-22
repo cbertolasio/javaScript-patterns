dff3ada0913da60bb8ebeeed4d43df0eee73f002
cbertolasio <cbertolasio@gmail.com>
2012-10-22 16:30:06 -0600

    added a module that uses knockout

---
+++ js/module-withko.js
@@ -0,0 +1,35 @@
+﻿/*globals $,alert*/
+
+MYAPP.namespace("MYAPP.Samples.Knockout");
+
+MYAPP.Samples.Knockout = (function () {
+    // initialize any dependencies, private properties, private methods, etc. in your var setup
+    var _name = { firstName: ko.observable("Joe"), lastName: ko.observable("Bloggs")},
+        _address = { address1: ko.observable("1900 Grant St"), city: ko.observable("Denver"), state: ko.observable("CO") },
+        computeFullName = function () { return _name.firstName() + " " + _name.lastName(); },
+        viewModel = {
+            name: _name,
+            address: _address,
+            fullName: computeFullName
+        },
+        documentReady = function () {
+            ko.applyBindings(viewModel);
+        };
+
+
+    // return the public api...
+    return {
+        ready : documentReady
+    };
+} ());
+
+
+$(document).ready(function () {
+    var koModule = MYAPP.Samples.Knockout;
+    koModule.ready();
+});
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 ﻿/*globals $,alert*/
 
 var BDS = BDS || {};
+var MYAPP = MYAPP || {};
+
+MYAPP.namespace = function (ns_string) {
+    var parts = ns_string.split("."),
+        parent = MYAPP,
+        i;
+
+    if (parts[0] === "MYAPP") {
+        parts = parts.slice(1);
+    }
+
+    for (i = 0; i < parts.length; i += 1) {
+        // create a property if it doesn't exist
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+};
 
 BDS.namespace = function (ns_string) {
     var parts = ns_string.split("."),
